06785c18688d69d6638aad274aab7085dabdafc3
SITA MAHALAKSHMI BHARATULA <103115856+maha2806@users.noreply.github.com>
2026-04-24 13:22:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Fade-in animation
+const elements = document.querySelectorAll('.fade-in');
+
+window.addEventListener('scroll', () => {
+    elements.forEach(el => {
+        const position = el.getBoundingClientRect().top;
+        const screenHeight = window.innerHeight;
+
+        if (position < screenHeight - 100) {
+            el.classList.add('show');
+        }
+    });
+});
